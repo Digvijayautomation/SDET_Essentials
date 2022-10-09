0497c294011f9f5c58afeef31814fc15452874de
Digvijay <dtikka6@gmail.com>
2022-10-09 11:00:25 +0530

    Fixture Factory

---
+++ src/main/java/Fixture_Factory_Library/Pojo.java
@@ -0,0 +1,21 @@
+package Fixture_Factory_Library;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter //Lombok will create the getter , setter methods  and constructors using this anotationsand so many other things automatically 
+@Setter
+@AllArgsConstructor
+public class Pojo {
+	
+	
+		
+		private int id;
+		private String name;
+		private String job;
+		private String createdAt;
+		
+
+	}
---
+++ src/test/java/Fixture_Factory_Test/Pojo_with_fixture_factory.java
@@ -0,0 +1,20 @@
+package Fixture_Factory_Test;
+
+import Fixture_Factory_Library.Pojo;
+import br.com.six2six.fixturefactory.*;
+
+
+// Using fixture factory we can create different type of templates like valid data, invalid data.
+
+public class Pojo_with_fixture_factory {
+	
+	//this is simple we can create object for pojo class and access
+	//Pojo emp=	new Pojo(20,"Digvijay","QA Lead","15-09-2022");
+	
+
+
+	// by using fixture factory we can do like below
+
+
+
+}
